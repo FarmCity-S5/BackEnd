21052ca20dc0f451bd987a82225c2c7a756dd211
mitiah <rakotoanadahymitia@gmail.com>
2024-01-26 08:22:19 +0300

    test changement git

---
+++ src/test/java/com/farmcity/farmcity/FarmcityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmcity.farmcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmcityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
